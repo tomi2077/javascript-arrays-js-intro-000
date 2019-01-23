c84ec4317a43d048cd46045ea14752b5840b5a75
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-23 22:53:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,6 @@ var addElementToBeginningOfArray = (array, element) => {
   return [element, ...array];
 }
 
+var destructivelyAddElementToBeginningOfArray = (array, element) =>{
+  return array.unshift(element)
+}
