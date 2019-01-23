1161c8176ecee0851aab491efa5c12b462d72e54
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-23 22:52:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
 
 var addElementToBeginningOfArray = (array, element) => {
-  array.push(element)
-  return array
+  return [element, ...array]
 }
 
