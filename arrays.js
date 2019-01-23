011faf0ea3ba6c2f77aaa300531ae0fa19e9cacc
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-23 23:23:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,6 @@ var destructivelyRemoveElementFromEndOfArray = (array) =>{
 }
 
 var removeElementFromEndOfArray = (array) => {
-  array.slice(0, array.length -1);
-  return array
+  return array.slice(0, array.length -1);
 }
 
